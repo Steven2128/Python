5c3d462e43cb269852c660e3c4f2a35cb06184e6
Stephen Alarcón <slj212885@gmail.com>
2019-12-05 20:57:48 -0500

    chore: Divisiones entre enteros y MOD

---
+++ Ejercicios/Tipos de  datos/Divisiones.py
@@ -0,0 +1,3 @@
+num1 = int(input("Ingrese un número: "))
+num2 = int(input("Ingrese otro número: "))
+print("{}".format(num1)+" entre {}".format(num2)+" da un cociente {}".format(num1//num2)+" y un resto {}".format(num1%num2))

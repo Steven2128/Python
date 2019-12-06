fd5c4b59ff3d5a2917ced2d281bde483cd93f822
Stephen Alarcón <slj212885@gmail.com>
2019-12-05 20:52:01 -0500

    chore: Round

---
+++ Ejercicios/Tipos de  datos/MasaCorporal.py
@@ -0,0 +1,6 @@
+# Masa corporal = Peso x Altura
+
+peso = float(input("Ingrese su peso: "))
+altura = float(input("Ingrese su altura: "))
+masa = peso * altura
+print("Tu indice de masa corporal es: {}".format(round(masa,2)))

0600dd71c729715572c6f570eef95d47f0b725e0
Stephen Alarcón <slj212885@gmail.com>
2019-12-09 22:04:22 -0500

    chore: Se agrega el triangulo

--- Platzi/Escalera.py
+++ Platzi/Escalera.py
@@ -1,6 +1,14 @@
 n = int(input("Ingrese un número: "))
-j= 6
-for i in range(1,n+1):
-    print("\t"*j+"# "*i)
-    j-=1
-    print("")
+i=1
+for j in range(n,0,-1):
+    print(" "*j+"*"*i)
+    i+=1
+
+n = int(input("Ingrese un número para el triangulo: "))
+
+i=1
+for j in range(n,0,-1):
+    print(" "*j+" *"*i)
+    i+=1
+
+  

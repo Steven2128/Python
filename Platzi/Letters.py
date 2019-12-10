92e65445509b2db5c7356a4495e68124de54b59a
Stephen Alarcón <slj212885@gmail.com>
2019-12-09 20:52:08 -0500

    chore: Ejercicios

--- Platzi/Letters.py
+++ Platzi/Letters.py
@@ -1,45 +1,14 @@
 def run():
     letters= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r",
     "s","t","u","v","x","y","z"]
-    count = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     text = str(input("Ingresa un texto: "))
     msg=""
     i=0 
     for i in letters:
-        print( text.find(i))
-    
-    for j in count:
-        print(j.)
+        if text.find(i)>=0:
+            count_letters = text.count(i)
+            msg += str(i)+str(count_letters)
+    print(msg)
 
-        
-        
-    
-
-    
-
-
-        
-
-
-"""
-if text.find("a") >=0:
-    msg +="a"+str(text.count("a"))
-else:
-    msg += "0"
-if  text.find("b") >=0:
-    msg+="b"+str(text.count("b"))
-else:
-    msg += "0"
-if  text.find("c") >=0:
-    msg +="c"+str(text.count("c"))
-else:
-    msg += "0"
-if  text.find("d") >=0:
-    msg +="d"+str(text.count("d"))
-else:
-    msg += "0"
-
-print(msg)
-"""
 if __name__ == "__main__":
     run()
--- Platzi/Vector.py
+++ Platzi/Vector.py
@@ -1,3 +1,4 @@
+import math
 class Vector():
     def __init__(self, i , j, k):
         self.i  = float(i)
@@ -26,25 +27,43 @@ class Vector():
         vector_multi = Vector(multi_i, multi_j, multi_k)
         return vector_multi
 
+def suma():
+    suma  = []
+    suma.append(vector_suma.i)
+    suma.append(vector_suma.j)
+    suma.append(vector_suma.k)
+    magnitud(suma)
+    print("Suma de los vectores: "+str(suma))
     
+
+def resta():
+    resta = []
+    resta.append(vector_resta.i)
+    resta.append(vector_resta.j)
+    resta.append(vector_resta.k)
+    print("Resta de los vectores: "+str(resta))
+
+def multi():
+    multi = []
+    multi.append(vector_multi.i)
+    multi.append(vector_multi.j)
+    multi.append(vector_multi.k)
+    print("La multiplicación de los vectores: "+str(multi))
+
+def magnitud(x):
+    suma = 0
+    for i in x:
+        suma+=float((i)**2)
+    print("La magnitud  del vector suma es: "+str(math.sqrt(suma)))
+
 v1 = Vector(1, 1, 1)
 v2 = Vector (5, -3, 4)
-vector_suma  = v1 + v2
-vector_resta = v1 - v2
-vector_multi = v1 * v2
-print("La magnitud  del ")
-suma  = []
-resta = []
-multi = []
-suma.append(vector_suma.i)
-suma.append(vector_suma.j)
-suma.append(vector_suma.k)
-print("Suma de los vectores: "+str(suma))
-resta.append(vector_resta.i)
-resta.append(vector_resta.j)
-resta.append(vector_resta.k)
-print("Resta de los vectores: "+str(resta))
-multi.append(vector_multi.i)
-multi.append(vector_multi.j)
-multi.append(vector_multi.k)
-print("La multiplicación de los vectores: "+str(multi))
+v3 = Vector(8, 2, -10)
+
+vector_suma  = v1 + v2 + v3
+vector_resta = v1 - v2 - v3
+vector_multi = v1 * v2 * v3
+
+suma()
+resta()
+multi()

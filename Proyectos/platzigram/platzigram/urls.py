efc9ab72f5fe394c3f934ba9ba79cfcbfabc9923
Stephen Alarcón <slj212885@gmail.com>
2020-01-08 17:28:59 -0500

    Creación de la primera app

--- Proyectos/platzigram/platzigram/urls.py
+++ Proyectos/platzigram/platzigram/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from platzigram import views as local_views
-
+from posts import views as posts_views
 
 
 urlpatterns = [
     path('hello-world/', local_views.hello_world),
-    path('<str:name>/<int:age>',local_views.say_hi)
+    path('<str:name>/<int:age>',local_views.say_hi),
+    path('posts/', posts_views.list_posts)
 ]
--- Proyectos/platzigram/posts/views.py
+++ Proyectos/platzigram/posts/views.py
@@ -1,3 +1,41 @@
-﻿from django.shortcuts import render
+﻿from django.http import HttpResponse
+from datetime import datetime
 
-# Create your views here.
+posts = [
+    {
+        "name" : "Mont Blac",
+        "user" : "Yésica Cortés",
+        "timestamp" : datetime.now().strftime("%dth %b %Y - %H:%M"),
+        "picture" : "https://i.picsum.photos/id/113/200/200.jpg"
+
+    },
+    {
+        'name': 'Mont Blac',
+        'user': 'Yésica Cortés',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1036'
+    },
+    {
+        'name': 'Via Láctea',
+        'user': 'C. Vander',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=903'
+    },
+    {
+        'name': 'Nuevo auditorio',
+        'user': 'Thespianartist',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1076'
+    }
+]
+
+
+def list_posts(request):
+    content = []
+    for post in posts:
+        content.append("""
+            <p><strong>{name}</strong></p>
+            <p><small>{user} - <i>{timestamp}</i></small></p>
+            <p><img src="{picture}"/></p>
+        """.format(**post))
+    return HttpResponse("<br>".join(content))

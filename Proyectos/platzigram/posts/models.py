76ed7b39f5b94411598fe1c24afc46cf0c912f14
Stephen Alarcón <slj212885@gmail.com>
2020-01-09 16:57:30 -0500

    Creacion de la clase User paracrear usuarios

--- Proyectos/platzigram/posts/models.py
+++ Proyectos/platzigram/posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    """User model"""
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    firts_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    
+    id_admin = models.BooleanField(default=False)
+    biografy = models.TextField()
+
+    birthdate = models.DateField(blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)

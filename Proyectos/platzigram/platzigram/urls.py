ca3c2a04502533d5978fad15ed1649ffb5ee2a28
Stephen Alarcón <slj212885@gmail.com>
2020-01-09 16:58:20 -0500

    Creación de la vista admin

--- Proyectos/platzigram/platzigram/urls.py
+++ Proyectos/platzigram/platzigram/urls.py
@@ -1,9 +1,11 @@
+from django.contrib import admin
 from django.urls import path
 from platzigram import views as local_views
 from posts import views as posts_views
 
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path('hello-world/', local_views.hello_world),
     path('<str:name>/<int:age>',local_views.say_hi),
     path('posts/', posts_views.list_posts)

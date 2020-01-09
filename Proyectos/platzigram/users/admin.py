437a12bb3e332db1cd5fd064d7f16514f5a40109
Stephen Alarcón <slj212885@gmail.com>
2020-01-09 17:59:40 -0500

    Jugando con el dashboard de admin

--- Proyectos/platzigram/users/admin.py
+++ Proyectos/platzigram/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from users.models import Profile
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """Profile admin"""
+    list_display = ("pk", "user", "phone_number", "website", "picture")
+    list_disply_links = ("pk", "user")
+    list_editable = ("phone_number", "website", "picture")

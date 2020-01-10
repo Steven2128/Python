2165cc71e56bbf8cde46b60e81c282864abb3c6e
Stephen Alarcón <slj212885@gmail.com>
2020-01-10 18:19:06 -0500

    Agregadas las funciones de login y logout con decoradores

--- Proyectos/platzigram/users/views.py
+++ Proyectos/platzigram/users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def login_view(request):
+    """Login a user"""
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('feed')
+        else:
+            return render(request, 'users/login.html', {'Error':'Invalid username or password'})
+
+    return render(request, 'users/login.html')
+
+@login_required
+def logout_view(request):
+    """Logout a user"""
+    logout(request)
+    return redirect('login')

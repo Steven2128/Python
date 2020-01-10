4618d6a7ffc04ccf46c9622fab57e8b648d7a021
Stephen Alarcón <slj212885@gmail.com>
2020-01-09 19:22:56 -0500

    Cambios en posts/

--- Proyectos/platzigram/posts/models.py
+++ Proyectos/platzigram/posts/models.py
@@ -1,17 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class User(models.Model):
-    """User model"""
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
+class Post(models.Model):
+    """Post model."""
 
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    
-    id_admin = models.BooleanField(default=False)
-    biografy = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
 
-    birthdate = models.DateField(blank=True, null=True)
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='posts/photos')
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        """Return title and username."""
+        return '{} by @{}'.format(self.title, self.user.username)

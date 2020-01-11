aee8d96a6f8df55dd9cd7a69ec949d2c3102e878
Stephen Alarcón <slj212885@gmail.com>
2020-01-10 19:50:50 -0500

    Agregada funcion de registrarse

--- Proyectos/platzigram/users/views.py
+++ Proyectos/platzigram/users/views.py
@@ -1,6 +1,14 @@
+#Django
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+
+#Exception
+from django.db import IntegrityError
+
+#Models
+from django.contrib.auth.models import User
+from users.models import Profile
 
 def login_view(request):
     """Login a user"""
@@ -22,3 +30,31 @@ def logout_view(request):
     """Logout a user"""
     logout(request)
     return redirect('login')
+
+
+def signup(request):
+    """signup view"""
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['password']
+        passwd_confirmation = request.POST['password_confirmation']
+
+        if passwd != passwd_confirmation:
+            return render(request, 'users/signup.html', {'Error': 'Password confirmation does not match'})
+        try:
+            user = User.objects.create_user(username=username, password=passwd)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'Error': 'Username is already in user'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
+

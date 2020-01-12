77745282cfe21b92b57888681c2a85235162bcf0
Stephen Alarcón <slj212885@gmail.com>
2020-01-12 16:29:14 -0500

    Models forms

--- Proyectos/platzigram/platzigram/views.py
+++ Proyectos/platzigram/platzigram/views.py
@@ -1,15 +1,39 @@
+"""Platzigram views."""
+
+# Django
 from django.http import HttpResponse
+
+# Utilities
 from datetime import datetime
-
-
-def say_hi(request, name, age):
-    if age<18:
-        message = "You wouldn't stay here"
-    else:
-        message = "Welcome"
-    return HttpResponse("Hi, "+name+"!. "+message)
+import json
 
 
 def hello_world(request):
-    now = datetime.now().strftime("%dth %b %Y - %H:%M")
-    return HttpResponse("Oh, hi. The time is "+str(now))
+    """Return a greeting."""
+    return HttpResponse('Oh, hi! Current server time is {now}'.format(
+        now=datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
+    ))
+
+
+def sort_integers(request):
+    """Return a JSON response with sorted integers."""
+    numbers = [int(i) for i in request.GET['numbers'].split(',')]
+    sorted_ints = sorted(numbers)
+    data = {
+        'status': 'ok',
+        'numbers': sorted_ints,
+        'message': 'Integers sorted successfully.'
+    }
+    return HttpResponse(
+        json.dumps(data, indent=4),
+        content_type='application/json'
+    )
+
+
+def say_hi(request, name, age):
+    """Return a greeting."""
+    if age < 12:
+        message = 'Sorry {}, you are not allowed here'.format(name)
+    else:
+        message = 'Hello, {}! Welcome to Platzigram'.format(name)
+    return HttpResponse(message)
